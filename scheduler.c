a13cd36924972921a029d4fdb0a4c8793acacca8
girraiffe <sargeanthost@gmail.com>
2023-02-06 23:09:03 -0500

    pseudo code for rr

--- scheduler.c
+++ scheduler.c
@@ -212,6 +212,95 @@ void analyze_SJF(struct job *pJob) {
            waitS / (float) id);
 }
 
+void policy_RR(struct job *pJob, int quanta) {
+    puts("Execution trace with RR:");
+    struct job *curJob = pJob;
+
+    struct job *queue[100] = {NULL};
+    struct job *jobs[100] = {NULL};
+    int cumTime = 0;
+    int numJobs = 0;
+    int runQueueIndex = 0;
+    int insertQueueIndex = 0;
+    int nextJobIndex = 0;
+
+
+    //check arrival time. if larger than current time, run next job from queue. increment time, and check job again. repeat
+
+    while (curJob != NULL) {
+        jobs[curJob->id] = curJob;
+        numJobs = curJob->id + 1;
+        curJob = curJob->next;
+    }
+    //while jobs[nextJobIndex] != null || queue[queueIndex] != null //if there exists a job
+//     if jobs[nextJobIndex]->arrival <= cumTime { //if there is a valid job non queued job
+//           currentJob = jobs[nextJobIndex]
+//           if (currentJob->duration >= quanta){
+//               runtime = quanta
+//           } else {
+//               runtime = nextJob->duration;
+//           }
+//           cumtime += runtime
+//           currentJob->duration = currentJob->duration-runtime
+
+//           if currentJob->duration > quanta {
+//              queue[insertQueueIndex++] = currentJob;
+//           }
+//           nextJobIndex++
+//     } else if queue[queueIndex] != null {//theres a job thats in the future, so run what we have instead. must check jobs before queue
+//          curJob = queue[runQueueIndex++]
+//          if (currentJob->duration >= quanta){
+//              runtime = quanta
+//           } else {
+//               runtime = nextJob->duration;
+//           }
+//
+//           currentJob->duration = currentJob->duration-runtime
+//
+//           if currentJob->duration > quanta {
+//              queue[insertQueueIndex++] = currentJob;
+//           }
+//          cumtime += runtime
+//          } else { //there must be a job that exists, but its arrival is in the future. seek to that arrival time.
+//           cumtime += jobs[nextJobIndex]->arrival - cumtime
+
+
+    for (int i = 0; i < numJobs; i++) {
+        //need to choose whether to use next job in jobs, next job in queue, or if none in queue and job is > cumTime,
+        //set cum time to the job by adding the difference between arrival and current to cum time, repeating
+        struct job *nextJob = jobs[i];
+
+        int runtime = 0;
+        if (nextJob->duration >= quanta){
+            runtime = quanta;
+        } else {
+            runtime = nextJob->duration;
+        }
+
+
+        printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]", cumTime, nextJob->id, nextJob->arrival, runtime);
+        cumTime += runtime;
+        nextJob->duration = nextJob->duration - quanta;
+    }
+
+
+
+//    while (curJob != NULL) {
+//        printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]");
+//        //just check > 0 for if job is finished, this can go negative
+//        curJob->duration -= sliceDuration;
+//        if (curJob->duration < 1){
+//            finishedJobs++;
+//        }
+//        curJob = curJob->next;
+//    }
+    puts("End of execution with RR.");
+}
+
+void analyze_RR(struct job *pJob, int duration) {
+
+}
+
 int main(int argc, char **argv) {
     if (argc < 4) {
         fprintf(stderr, "missing variables\n");
@@ -222,7 +311,7 @@ int main(int argc, char **argv) {
     int analysis = atoi(argv[1]);
     char *policy = argv[2],
             *workload = argv[3];
-//    int slice_duration = atoi(argv[4]);
+    int slice_duration = atoi(argv[4]);
 
     // Note: we use a global variable to point to
     // the start of a linked-list of jobs, i.e., the job list
@@ -246,8 +335,14 @@ int main(int argc, char **argv) {
         }
         exit(EXIT_SUCCESS);
     }
-    // TODO: Add other policies
-
+    if (strcmp(policy, "RR") == 0) {
+        policy_RR(head, slice_duration);
+        if (analysis) {
+            printf("Begin analyzing SJF:\n");
+            analyze_RR(head, slice_duration);
+            printf("End analyzing SJF.\n");
+        }
+        exit(EXIT_SUCCESS);
+    }
     exit(EXIT_SUCCESS);
 }
-
